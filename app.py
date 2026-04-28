bef9f57bad41adf53d79dc149ddbea9aa0e94ad2
Adryan Rafael <Adryan.campos@escola.pr.gov.br>
2026-04-28 09:53:10 -0300

    Versão Melhorada

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+VERMELHO = "\033[31m"
+VERDE = "\033[32m"
+RESET = "\033[0m"
+
 from models.jogo import Jogo
 from services.jogo_service import salvar_jogos, carregar
 jogo = carregar()
@@ -6,7 +10,7 @@ print("=== SISTEMA DE JOGOS ===")
 
 while True:
     print("\n1 - Adicionar jogo")
-    print("2 - Listar jogos")
+    print("2 - Listar jogos")   
     print("3 - Sair")
 
     opcao = input("Escolha: ")
@@ -15,13 +19,32 @@ while True:
         # TODO: criar jogo
         print("Criar jogo")
         nome = input("Nome: ")
-        genero = input("Genero: ")
-        ano = input("Ano: ")
+
+        while True:
+            genero = input("Gênero: ")
+            if genero.replace(" ", "").isalpha() and len(genero) > 0:
+                break
+            else:
+                print(VERMELHO + "Erro: Gênero deve conter apenas letras." + RESET)
+
+        while True:
+            try:
+                entrada_ano = input("Ano: ")
+                ano = int(entrada_ano)
+                
+                if len(entrada_ano) == 4:
+                    break
+                else:
+                    print(VERMELHO + "Erro: Digite um ano válido com 4 dígitos (ex: 2026)." + RESET)
+                
+            except ValueError:
+                print(VERMELHO + "Erro: Digite apenas números para o ano!" + RESET)
+
 
         novo_jogo = Jogo(nome, genero, ano)
         jogo.append(novo_jogo)
         salvar_jogos(jogo)
-        print("Jogo cadastrado!")
+        print(VERDE + "Jogo cadastrado!" + RESET)
 
     elif opcao == "2":
         # TODO: listar jogos
